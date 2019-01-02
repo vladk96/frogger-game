eebb4bfa131618fe18711ffc346f312f22d7add1
vladk96 <vladk961@gmail.com>
2019-01-02 19:12:26 +0200

    add project

--- js/app.js
+++ js/app.js
@@ -1,39 +1,87 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var PLAYER_INIT_POS = {
+    x: 200,
+    y: 350
+} 
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    if (this.x <= 550) {
+        this.x = this.x + this.speed;
+    } else {
+        this.x = 0;
+        this.speed = getRandomNumber();
+    }
+
+    this.handlesCollision();
 };
 
-// Draw the enemy on the screen, required method for game
+Enemy.prototype.handlesCollision = function () {
+    if (player.x < this.x + 60 && this.x < player.x + 65 &&
+        player.y < this.y + 70 && this.y < player.y + 75) {
+        showMessage(false);
+        player.x = PLAYER_INIT_POS.x;
+        player.y = PLAYER_INIT_POS.y;
+    }
+}
+
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function(dt) {};
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
+Player.prototype.handleInput = function(direction) {
+    if (direction === 'up' && this.y <= 0) {
+        showMessage(true);
+        this.x = PLAYER_INIT_POS.x;
+        this.y = PLAYER_INIT_POS.y;
+    }
 
+    if (direction === 'left' && this.x > 0) {
+        this.x -= 100;
+    }
+    if (direction === 'up' && this.y > 0) {
+        this.y -= 50;
+    }
+    if (direction === 'right' && this.x < 400) {
+        this.x += 100;
+    }
+    if (direction === 'down' && this.y < 400) {
+        this.y += 50;
+    }
+}
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+var getRandomNumber = function() {
+    return Math.floor(Math.random() * Math.floor(8 - 2) + 2);
+}
+
+var showMessage = function(boolean) {
+    boolean ? alert('You won!') : alert('You lost!');
+}
+
+var player = new Player(PLAYER_INIT_POS.x, PLAYER_INIT_POS.y);
+var enemy1 = new Enemy(-50, 210, getRandomNumber());
+var enemy2 = new Enemy(-50, 130, getRandomNumber());
+var enemy3 = new Enemy(-50, 60, getRandomNumber());
+
+var allEnemies = [enemy1, enemy2, enemy3];
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
